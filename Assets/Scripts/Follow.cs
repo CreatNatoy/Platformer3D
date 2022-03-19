0a80c9f33206b73bc62176074934d3b5d996e346
CreatNatoy <76531899+CreatNatoy@users.noreply.github.com>
2022-03-19 18:45:34 +0100

    Move and Camare
    The movement of physics.
    Camera tracking of the player.

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speedLerp;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _speedLerp); 
+    }
+
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _maxSpeed;
     [SerializeField] private float _jumpedForce;
     [SerializeField] private float _angleGroundJump = 45f; 
     [SerializeField] private float _friction;
@@ -24,12 +25,24 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move(); 
+        Move();
     }
 
     private void Move()
     {
-        _rigidbody.AddForce(Input.GetAxis("Horizontal") * _moveSpeed, 0, 0, ForceMode.VelocityChange);
+        float speedMultuplier = 1f;
+
+        if (!_isGround)
+            speedMultuplier = 0.2f;
+        
+        if (_rigidbody.velocity.x > _maxSpeed && Input.GetAxis("Horizontal") > 0)
+            speedMultuplier = 0f;
+        if (_rigidbody.velocity.x < -_maxSpeed && Input.GetAxis("Horizontal") < 0)
+            speedMultuplier = 0f;
+
+        _rigidbody.AddForce(Input.GetAxis("Horizontal") * _moveSpeed * speedMultuplier, 0, 0, ForceMode.VelocityChange);
+
+        if(_isGround)
         ResistanceVelocity();
     }
 
